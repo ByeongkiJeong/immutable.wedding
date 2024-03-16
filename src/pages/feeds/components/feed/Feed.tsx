29bde011fad562ea37e8af6c31ff5eb473388328
Byeongki Jeong <byeongkij@azurelabs.io>
2024-03-17 00:42:13 +0900

    last...?

--- src/pages/feeds/components/feed/Feed.tsx
+++ src/pages/feeds/components/feed/Feed.tsx
@@ -104,18 +104,15 @@ function FeedItemContainer({
       </div>
       <SSRSuspense fallback={null}>
         <CommentWrapper>
-          {isInputMode ? (
-            <CommentForm id={id} onSubmit={toDisplayMode} />
-          ) : (
-            <Comments
-              id={id}
-              inputModeButton={
-                <SmallGrayButton onClick={handleCommentIconClick}>
-                  댓글 남기기
-                </SmallGrayButton>
-              }
-            />
-          )}
+          <CommentForm id={id} onSubmit={toDisplayMode} />
+          <Comments
+            id={id}
+            inputModeButton={
+              <SmallGrayButton onClick={handleCommentIconClick}>
+                댓글 남기기
+              </SmallGrayButton>
+            }
+          />
         </CommentWrapper>
       </SSRSuspense>
     </Wrapper>
